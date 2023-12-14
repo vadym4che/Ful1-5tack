1465e1cd4572f41788d1d8fc32025396036ea1f9
vadym4che <vadym4che@gmail.com>
2023-12-14 20:33:48 +0200

    Updates

---
+++ assets/AboutMe-SG7Q9vEl.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as c}from"./index-oNFmQoyy.js";const s={},n=c("h2",null," About Me ",-1),_=[n];function a(r,l){return o(),t("div",null,_)}const u=e(s,[["render",a]]);export{u as default};
---
+++ assets/MyWorks-S3v18xSf.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as c}from"./index-oNFmQoyy.js";const t={},n=c("h2",null," My Works ",-1),r=[n];function _(a,l){return o(),s("div",null,r)}const f=e(t,[["render",_]]);export{f as default};
