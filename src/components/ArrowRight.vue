488518488fa47f5c6ac988a68f2a07ddc66c439b
vadym4che <vadym4che@gmail.com>
2024-01-05 07:27:33 +0200

    arrow-right stroke width 2 -> 3

--- src/components/ArrowRight.vue
+++ src/components/ArrowRight.vue
@@ -1,7 +1,7 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" viewBox="0 0 18 12" fill="none" class="arrow-right" :style="{ height, width: parseInt(height) * 1.5 + 'rem' }">
-    <path d="M1 5.5H17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M12 1L17 6L12 11" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1 5.5H17" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 1L17 6L12 11" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 </template>
 
